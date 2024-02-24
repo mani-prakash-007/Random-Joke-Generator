9d2b06b73696eba6f3c7929e2b8818a6af30c11c
mani-prakash-007 <manip8072@gmail.com>
2024-02-24 21:00:12 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,13 @@
+const form = document.querySelector('form');
+const results = document.querySelector('results');
+
+form.addEventListener('submit' , (event) => {
+    event.preventDefault();
+    GenerateJoke();
+})
+
+const GenerateJoke = async() => {
+    const url = await fetch(``);
+    const respone = await
+}
+
